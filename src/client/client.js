69415599d4e213187dca1f219d72e0fd6e6f03c9
Branko Culum <brankoculum@users.noreply.github.com>
2018-07-17 15:14:17 -0400

    Added player chip overlay when mouse moves

--- src/client/client.js
+++ src/client/client.js
@@ -2,6 +2,7 @@ import { renderer, stage } from './renderer.js';
 import Chip from '../shared/bodies/Chip';
 import engine from './engine.js';
 
+// On click, add a chip at the mouse's x and y relative to canvas
 document.querySelector('canvas').addEventListener('click', (e) => {
   e.preventDefault()
 
@@ -12,5 +13,15 @@ document.querySelector('canvas').addEventListener('click', (e) => {
 
   renderer.render(stage);
 })
+
+// // When the client moves the mouse, display a chip overlay
+// document.querySelector('canvas').addEventListener('mousemove', (e) => {
+//   const x = e.offsetX;
+//   const y = e.offsetY;
+//
+//   const chip = new Chip({ x, y });
+//   chip.addToRenderer(stage);
+//   renderer.render(stage)
+// })
 
 renderer.render(stage);
