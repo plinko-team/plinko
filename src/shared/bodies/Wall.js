79a3efcc7959b7f2bcec45b172c89728dea09d42
Josh Nelson <joshcdnelson@gmail.com>
2018-07-17 09:50:04 -0700

    Created wall classes that are generated in setup

--- src/shared/bodies/Wall.js
+++ src/shared/bodies/Wall.js
@@ -1,18 +1,70 @@
-let leftWall = new PIXI.Graphics()
-leftWall.beginFill(0xff22aa)
-leftWall.drawRect(0, 100, 5, 500)
-leftWall.endFill()
+import { Bodies, World } from 'matter-js'
+import { PEG_FRICTION, PEG_RESTITUTION} from '../constants/bodies'
 
-let rightWall = new PIXI.Graphics()
-leftWall.beginFill(0xff22aa)
-leftWall.drawRect(795, 100, 5, 500)
-leftWall.endFill()
+let PIXI;
 
-let ground = new PIXI.Graphics()
-ground.beginFill(0xff22aa)
-ground.drawRect(0, 590, 800, 10)
-ground.endFill()
-stage = new PIXI.Container()
+if (typeof window === 'object') {
+  PIXI = require('pixi.js');
+}
 
-stage.addChild(leftWall, rightWall, ground)
-renderer.render(stage);
+class Wall {
+  constructor({x, y, width, height}) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.createPhysics({ width, height });
+    if (typeof window === 'object') { this.createSprite() };
+  }
+
+  createPhysics({width, height}) {
+    let options = {
+      restitution: PEG_RESTITUTION,
+      friction: PEG_FRICTION,
+    }
+   
+    this.body = Bodies.rectangle(this.x, this.y, this.width, this.height, options);
+    this.body.isStatic = true;
+    this.body.position.x = this.x;
+    this.body.position.y = this.y;
+    this.body.label = this.type;
+    this.body.isShrinking = false;
+  }
+
+  createSprite() {
+    this.sprite = new PIXI.Graphics()
+    this.sprite.beginFill(0xff22aa);
+    this.sprite.drawRect(this.x, this.y, this.width, this.height);
+    this.sprite.endFill();
+  }
+
+  addToRenderer(stage) {
+    stage.addChild(this.sprite);
+  }
+  
+  addToEngine(world) {
+    World.add(world, this.body);
+  }
+}
+
+export class VerticalWall extends Wall {
+  constructor({x, y, width, height}) {
+    super({x, y, width, height});
+  }
+}
+
+export class HorizontalWall extends Wall {
+  constructor() {
+    super({ x: 0,
+            y: 590,
+            width: 800,
+            height: 10
+    })
+  }
+}
+
+export class BucketWall extends Wall {
+  constructor({x, y, width, height}) {
+    super({x, y, width, height});
+  }
+}
---
+++ src/shared/constants/bodies.js
@@ -0,0 +1,2 @@
+export const PEG_FRICTION = 0
+export const PEG_RESTITUTION = 0.5
