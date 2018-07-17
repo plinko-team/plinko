0cb637f6be63d9b4d12f2ed3443047d36bd876c3
Ryann McQuilton <20259631+ryannmcq@users.noreply.github.com>
2018-07-17 15:26:38 -0700

    Adjust canvas and drop spacing

--- src/shared/constants/canvas.js
+++ src/shared/constants/canvas.js
@@ -2,6 +2,12 @@ import { CHIP_DIAMETER } from './bodies'
 
 export const CANVAS_WIDTH = 800;
 export const CANVAS_HEIGHT = 600;
+
 export const ROWS = 6;
-export const COLS = 11;
-export const SPACING = CANVAS_WIDTH / COLS;
+export const ROW_SPACING = (CANVAS_HEIGHT / ROWS) * .65;
+
+export const COLS = 12;
+export const COL_SPACING = CANVAS_WIDTH / COLS;
+
+export const VERTICAL_OFFSET = ROW_SPACING / 2;
+export const HORIZONTAL_OFFSET = COL_SPACING / 2;
