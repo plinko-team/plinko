55b88b58f27ff6a9dae82e667555ce1b792cfae3
Branko Culum <brankoculum@users.noreply.github.com>
2018-07-17 18:21:38 -0400

    Added rotation to chips

--- src/shared/constants/bodies.js
+++ src/shared/constants/bodies.js
@@ -1,11 +1,8 @@
-export const PEG_FRICTION = 0;
+export const PEG_FRICTION = 1;
 export const PEG_RESTITUTION = 0.5;
 export const PEG_RADIUS = 5;
-export const CHIP_FRICTION = 0;
+export const CHIP_FRICTION = 1;
 export const CHIP_RESTITUTION = 0.5;
-export const CHIP_RADIUS = 15;
-export const WALL_FRICTION = 0;
+export const CHIP_RADIUS = 20;
+export const WALL_FRICTION = 1;
 export const WALL_RESTITUTION = 0.5;
-
-
-
