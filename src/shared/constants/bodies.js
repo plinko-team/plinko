2836cdd344785dc98e556470b8a29b271f724f92
Ryann McQuilton <20259631+ryannmcq@users.noreply.github.com>
2018-07-17 15:36:29 -0700

    Add chip diameter constant

--- src/shared/constants/bodies.js
+++ src/shared/constants/bodies.js
@@ -4,5 +4,6 @@ export const PEG_RADIUS = 5;
 export const CHIP_FRICTION = 1;
 export const CHIP_RESTITUTION = 0.5;
 export const CHIP_RADIUS = 20;
+export const CHIP_DIAMETER = CHIP_RADIUS * 2;
 export const WALL_FRICTION = 1;
 export const WALL_RESTITUTION = 0.5;
