bc88ec95afc0c1825dfaec580606c286a6e2d937
Branko Culum <brankoculum@users.noreply.github.com>
2018-07-17 20:56:46 -0400

    Pegs change color upon collision; also webpack set to development

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,12 @@ var path = require('path');
 var fs = require('fs');
 
 module.exports = {
+  mode: 'development',
+  watch: true,
+  watchOptions: {
+    ignored: [/node_modules/],
+    poll: 1000,
+  },
   target: 'web',
   entry: './src/client/client.js',
   output: {
