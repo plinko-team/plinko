1cb985d62b34b112c12f6b4df01152d257817a88
Ryann McQuilton <20259631+ryannmcq@users.noreply.github.com>
2018-07-18 13:02:45 -0700

    Import constants

--- src/shared/constants/game.js
+++ src/shared/constants/game.js
@@ -1,3 +1,6 @@
+import { VERTICAL_MARGIN } from './canvas';
+import { CHIP_RADIUS, PEG_RADIUS } from './bodies';
+
 export const MS_IN_SECOND = 1000;
 export const FPS = 60;
 export const TIMESTEP = MS_IN_SECOND / FPS;
