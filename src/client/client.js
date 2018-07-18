150907ee02df1bf40ce18c09bc8b40cbbe30950a
Branko Culum <brankoculum@users.noreply.github.com>
2018-07-18 17:58:00 -0400

    Added debug renderer option to client.js

--- src/client/client.js
+++ src/client/client.js
@@ -1,6 +1,16 @@
 import Game from './game'
 import engine from './engine';
 import { renderer, stage } from './renderer';
-
+import { Render } from 'matter-js'
 const game = new Game({ renderer, engine, stage })
 game.init().start();
+
+// Debug renderer
+if (false) {
+  const r = Render.create({
+    element: document.body,
+    engine: engine,
+  })
+
+  Render.run(r)
+}
