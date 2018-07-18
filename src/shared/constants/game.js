4983157bbe87789fd04f877da267a288366850d4
Ryann McQuilton <20259631+ryannmcq@users.noreply.github.com>
2018-07-18 13:02:18 -0700

    Fix drop boundary
    -make drop boundary constant responsive to vertical margin and chip/peg size

--- src/shared/constants/game.js
+++ src/shared/constants/game.js
@@ -3,4 +3,4 @@ export const FPS = 60;
 export const TIMESTEP = MS_IN_SECOND / FPS;
 
 // Cannot drop chips under DROP_BOUNDARY px from top
-export const DROP_BOUNDARY = 50
+export const DROP_BOUNDARY = VERTICAL_MARGIN - CHIP_RADIUS - PEG_RADIUS;
