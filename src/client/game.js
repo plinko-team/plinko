5604aa6982230b30288a1a89b25766855a07bf75
Branko Culum <brankoculum@users.noreply.github.com>
2018-07-18 15:33:13 -0400

    Extracted event callbacks in Game Class

--- src/client/game.js
+++ src/client/game.js
@@ -19,51 +19,56 @@ export default class Game {
     return this;
   }
 
+  onClick = (e) => {
+    e.preventDefault();
+
+    // Short circuit handler if outside of drop boundary
+    if (e.offsetY > DROP_BOUNDARY) { return }
+
+    const chip = new Chip({ x: e.offsetX, y: e.offsetY });
+
+    chip.addToEngine(this.engine.world);
+    chip.addToRenderer(this.stage);
+    chip.registerUpdateListener(this.engine);
+    this.renderer.render(this.stage);
+  }
+
+  onMouseEnter = (e) => {
+    const x = e.offsetX;
+    const y = e.offsetY;
+    const hoverChip = new HoverChip({ x, y });
+    hoverChip.addToRenderer(this.stage);
+
+    e.target.addEventListener('mouseleave', () => {
+      hoverChip.removeChip(this.stage);
+    });
+  }
+
+  onCollisionStart = (event) => {
+    const pairs = event.pairs;
+
+    for (let i = 0; i < pairs.length; i++) {
+      let pair = pairs[i];
+
+      if (pair.bodyA.label === 'peg') {
+        pair.bodyA.sprite.tint = 0xFFAAAA;
+      } else if (pair.bodyB.label === 'peg') {
+        pair.bodyB.sprite.tint = 0xFFAAAA;
+      }
+    }
+  }
+
   registerEvents() {
     // On click, add a chip at the mouse's x and y relative to canvas
-    document.querySelector('canvas').addEventListener('click', (e) => {
-      e.preventDefault();
-
-      // Short circuit handler if outside of drop boundary
-      if (e.offsetY > DROP_BOUNDARY) { return }
-
-      const chip = new Chip({ x: e.offsetX, y: e.offsetY });
-
-      chip.addToEngine(this.engine.world);
-      chip.addToRenderer(this.stage);
-      chip.registerUpdateListener(this.engine);
-      this.renderer.render(this.stage);
-    });
-
+    document.querySelector('canvas').addEventListener('click', this.onClick);
 
     // When the client moves the mouse, display a chip overlay
-    document.querySelector('canvas').addEventListener('mouseenter', (e) => {
-      const x = e.offsetX;
-      const y = e.offsetY;
-      const hoverChip = new HoverChip({ x, y });
-      hoverChip.addToRenderer(this.stage);
-
-      e.target.addEventListener('mouseleave', () => {
-        hoverChip.removeChip(this.stage);
-      });
-    })
+    document.querySelector('canvas').addEventListener('mouseenter', this.onMouseEnter)
   }
 
   registerPhysicsEvents() {
     // Collision Events
-    Events.on(this.engine, 'collisionStart', function(event) {
-      const pairs = event.pairs;
-
-      for (let i = 0; i < pairs.length; i++) {
-        let pair = pairs[i];
-
-        if (pair.bodyA.label === 'peg') {
-          pair.bodyA.sprite.tint = 0xFFAAAA;
-        } else if (pair.bodyB.label === 'peg') {
-          pair.bodyB.sprite.tint = 0xFFAAAA;
-        }
-      }
-    });
+    Events.on(this.engine, 'collisionStart', this.onCollisionStart);
   }
 
   start() {
