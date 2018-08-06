5ae6958427110ac34026f26e4d80cd90f08e508f
Branko Culum <brankoculum@users.noreply.github.com>
2018-08-06 18:23:42 -0400

    Fixed a typo which accidentally removed the 'game started' event callback and caused an error in the inspector
    Co-authored-by: Ryann McQuilton <20259631+ryannmcq@users.noreply.github.com>
    Co-authored-by: Josh Nelson <22858810+JoshCNelson@users.noreply.github.com>

--- client/src/components/GameContainer.js
+++ client/src/components/GameContainer.js
@@ -47,7 +47,7 @@ export default class GameContainer extends Component {
   componentWillUnmount() {
     if (this.hasOpenSocket()) {
       console.log('emitting leave game')
-      this.props.socket.emit('leave game');
+      this.props.socket.emit('leave game', {});
       this.unregisterSocketEvents();
       clearInterval(this.interval);
     }
@@ -55,7 +55,7 @@ export default class GameContainer extends Component {
 
   handleStartGameClick = () => {
     console.log('This should start a countdown then move all current users into game');
-    this.props.socket.emit('start game');
+    this.props.socket.emit('start game', {});
   }
 
   handleUserJoin = (name) => {
@@ -104,11 +104,11 @@ export default class GameContainer extends Component {
       }, 1000);
     })
 
-    socket.on('game started'), () => {
-      console.log("Game started event, set gameInProgress to true")
+    socket.on('game started', () => {
+      console.log("Game started event, set gameInProgress to true");
 
-      this.setState({ gameInProgress: true })
-    }
+      this.setState({ gameInProgress: true });
+    })
 
     socket.on('game over', () => {
       console.log("Game over event; gameIsRunning and gameInProgress to false")
