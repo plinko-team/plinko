2fab4a5b5bf94e4cc8cda59165ce9e7169455f10
Branko Culum <brankoculum@users.noreply.github.com>
2018-08-15 16:29:03 -0400

    Changed frame estimate to round the latency instead of ceil

--- client/src/game/clientEngine.js
+++ client/src/game/clientEngine.js
@@ -123,7 +123,7 @@ export default class ClientEngine {
 
     this.socket.on(SNAPSHOT, ({ frame, chips, pegs, score, winner, targetScore }) => {
       // let { chips, pegs, score, winner, targetScore } = Serializer.decode(encodedSnapshot);
-      let estimatedServerFrame = frame + Math.ceil(this.latency / TIMESTEP);
+      let estimatedServerFrame = frame + Math.round(this.latency / TIMESTEP);
 
       this.nextWholeFrame = estimatedServerFrame
 
