d24090155e15720b5ab21eff41160bb5eb659ab4
Ryann McQuilton <20259631+ryannmcq@users.noreply.github.com>
2018-08-23 11:58:51 -0700

    Wired nav buttons

--- client/src/components/TopNav.js
+++ client/src/components/TopNav.js
@@ -1,24 +1,39 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { WiredButton } from 'wired-elements';
+
+const buttonStyles = {
+
+}
 
 const TopNav = () => {
   return (
     <div className="nav-container">
       <nav className="row">
         <div className="two columns offset-by-two">
-          <NavLink to="about" className="button ">About</NavLink>
+          <NavLink to="about">
+            <wired-button style={buttonStyles} elevation='2'>Case Study</wired-button>
+          </NavLink>
         </div>
         <div className="two columns">
-          <NavLink to="play" className="button">Play</NavLink>
+          <NavLink to="play">
+            <wired-button style={buttonStyles} elevation='2'>Play</wired-button>
+          </NavLink>
         </div>
         <div className="two columns">
-          <NavLink to="help" className="button">Help</NavLink>
+          <NavLink to="help">
+            <wired-button style={buttonStyles} elevation='2'>Help</wired-button>
+          </NavLink>
         </div>
         <div className="two columns">
-          <NavLink to="team" className="button">Team</NavLink>
+          <NavLink to="team">
+            <wired-button style={buttonStyles} elevation='2'>The Team</wired-button>
+          </NavLink>
         </div>
         <div className="two columns">
-          <a href="https://github.com/plinko-team/plinko" className="button">GitHub</a>
+          <a href="https://github.com/plinko-team/plinko">
+            <wired-button style={buttonStyles} elevation='2'>Github</wired-button>
+          </a>
         </div>
       </nav>
     </div>
