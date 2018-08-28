348fd124d8f790c2023c1f2381da9c31c7b0ebaa
Ryann McQuilton <20259631+ryannmcq@users.noreply.github.com>
2018-08-28 13:29:42 -0700

    Polyfill custom elements for Firefox

--- client/src/components/StartGameButton.js
+++ client/src/components/StartGameButton.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { WiredButton } from 'wired-elements';
+import { WiredButton } from 'wired-button';
 
 const StartGameButton = ({ handleClick }) => {
   return (
