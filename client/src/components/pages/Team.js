57a5c45848af123f072cb8c8252b6ebe31d86df7
Josh Nelson <joshcdnelson@gmail.com>
2018-08-31 18:17:56 -0700

    Added images and links structure for the team tab

--- client/src/components/pages/Team.js
+++ client/src/components/pages/Team.js
@@ -5,6 +5,40 @@ const Team = () => {
     <main>
       <div className="main-content">
         <h1>The Team</h1>
+        <div>
+          <ul className="team">
+            <li>
+              <a href="#">
+                <figure>
+                  <img src="https://via.placeholder.com/200x200" alt="Branko Culum"/>
+                </figure>
+                <strong>Branko Culum</strong>
+                  <br />
+                  Software Engineer
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <figure>
+                  <img src="https://via.placeholder.com/200x200" alt="Ryann McQuilton"/>
+                </figure>
+                <strong>Ryann McQuilton</strong>
+                  <br />
+                  Software Engineer
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <figure>
+                  <img src="https://i.imgur.com/2ZDRwrh.jpg" alt="Josh Nelson"/>
+                </figure>
+                <strong>Josh Nelson</strong>
+                  <br />
+                  Software Engineer
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </main>
   )
