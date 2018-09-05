8085ef4fcfb7339cf3ba60f83136e2c51609ea83
Branko Culum <brankoculum@users.noreply.github.com>
2018-09-05 14:55:32 -0400

    master-fix-invalid-score

--- client/src/components/ScoreBar.js
+++ client/src/components/ScoreBar.js
@@ -30,7 +30,7 @@ export default class ScoreBar extends Component {
       document.querySelector(`.player-${this.props.playerId} wired-card`).requestRender();
     }
 
-    if (this.props.targetPercentage !== prevProps.targetPercentage) {
+    if (!isNaN(this.props.targetPercentage) && this.props.targetPercentage !== prevProps.targetPercentage) {
       this.setState({
         // difference between 100% and current target percentage, as a negative
         targetBarLeftPosition: -(100 - this.props.targetPercentage) + '%',
